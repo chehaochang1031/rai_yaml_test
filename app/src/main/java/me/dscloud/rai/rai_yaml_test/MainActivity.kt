286a9cc95a788f582627f7cd757bca3ad74a281a
blake <blake@rai.dscloud.me>
2022-01-19 14:55:01 +0800

    first

---
+++ app/src/main/java/me/dscloud/rai/rai_yaml_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.dscloud.rai.rai_yaml_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
